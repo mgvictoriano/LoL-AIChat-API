523e0a35d347872f6cb3814120e0e085c24913c1
Michelle Victoriano <desenvolvimento12@sifat.com.br>
2024-04-01 16:43:47 -0300

    refactor: otimização de metodo askChampion

--- src/main/java/tech/mgaia/LoLChat/application/AskChampionUseCase.java
+++ src/main/java/tech/mgaia/LoLChat/application/AskChampionUseCase.java
@@ -1,19 +1,26 @@
 package tech.mgaia.LoLChat.application;
 
+import tech.mgaia.LoLChat.domain.exception.ChampionNotFoundException;
 import tech.mgaia.LoLChat.domain.model.Champion;
 import tech.mgaia.LoLChat.domain.ports.ChampionsRepository;
+import tech.mgaia.LoLChat.domain.ports.GenerativeAiService;
 
-import java.util.List;
 
-
-public record ListChampionsUseCase(ChampionsRepository repository) {
+public record AskChampionUseCase(ChampionsRepository repository, GenerativeAiService genAiService) {
 
     public String askChampion(Long idChampion, String question) {
 
-        Champion Champion = repository.findById(idChampion).orElseThrow(() -> new ChampionNotFoundException(idChampion));
+        Champion champion = repository.findById(idChampion)
+                .orElseThrow(() -> new ChampionNotFoundException(idChampion));
 
-        return champion.generateContextByQuestion(question);
+        String context = champion.generateContextByQuestion(question);
+        String objective = """
+                Atue como um assistente com a habilidade de se comportar como os Campeões do League of Legends (LOL).
+                Responsa perguntas incorporando a personalidade e estilo de um determinado Campeão.
+                Segue a pergunta, o nome do Campeão e sua respectiva lore (história):
+                
+                """;
+
+        return genAiService.generateContent(objective, context);
     }
-
-
 }
